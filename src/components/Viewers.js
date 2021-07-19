e98fccabd16487955630ea7283c469cd0936962e
Himanshu4776 <hk91619@gmail.com>
2021-07-19 16:39:28 +0530

    ADD:Viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,11 +1,56 @@
 import React from 'react'
+import styled from 'styled-components';
+import { wrap } from 'yargs';
 
 function Viewers() {
     return (
-        <div>
-            Viewers
-        </div>
+        <Container>
+            <Wrap style={{backgroundImage:`url(${'./videos/1564674844-disney.mp4'})`}}>
+                <img src = "/images/viewers-disney.png"></img>
+            </Wrap>
+            <Wrap>
+                <img src = "/images/viewers-pixar.png"></img>
+            </Wrap>
+            <Wrap>
+                <img src = "/images/viewers-marvel.png"></img>
+            </Wrap>
+            <Wrap>
+                <img src = "/images/viewers-starwars.png"></img>
+            </Wrap>
+            <Wrap>
+                <img src = "/images/viewers-national.png"></img>
+            </Wrap>
+        </Container>
     )
 }
-
+  
 export default Viewers;
+
+const Container = styled.div `
+    margin-top: 30px;
+    display: grid;
+    grid-gap: 25px;
+    padding: 30px 0 26px;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+`
+const Wrap = styled.div `
+border: 3px solid rgba(249,249,249,0.1);
+border-radius: 10px;
+cursor: pointer;
+box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+transition: all 250ms cubic-beizer(0.25, 0.46, 0.45, 0.94) 0s;
+
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+// this little & concat the css with the parent element of tags.
+// It means tag:hover.
+
+&:hover {
+    transform: scale(1.5);
+    border-color: rgba(249, 249, 249, 0.8);
+}
+`
